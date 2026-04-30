0782c8456ff2128abf53fd0ee57bf410f7762971
lucas <lucasnotaroberto2@gmail.com>
2026-04-30 16:45:25 -0300

    yes

--- projeto.py
+++ projeto.py
@@ -1,5 +1,7 @@
 #parte 1 do projeto - definição de interface inicial(cadastro/login)
 
+import ast
+
 #--------cadastro---------------------------------------------------------------------------
 def novo_usuario() :
     nome = input ("digite seu nome...: ")
@@ -11,39 +13,55 @@ def novo_usuario() :
     return usuario
 
 def criar_lista_usuarios() :
+    usu = []
     arquivo = open("usuarios.txt","r")
-    arquivo.seek(0)
-    conteudo = arquivo.read()
-    arquivo.close()
-    if conteudo == "":
-        return []
-    return eval(conteudo)
+    for i in arquivo.readlines():
+        usu.append(i)
+    return usu
 
 def adicionar_usuario(usuario_criado) :
     usuarios = criar_lista_usuarios()
     usuarios.append(usuario_criado)
     arquivo = open("usuarios.txt","w")
-    arquivo.write(str(usuarios))
+    for i in usuarios:
+        arquivo.write("\n%s" %i)
     arquivo.close()
 
 #--------login------------------------------------------------------------------------------
 
-def login_usuario() :
+def checagem_login() :
     email = input("digite seu email..: ")
     senha = input("digite sua senha..: ")
-    return email,senha
-
-def checagem_login(usuario_logado) :
     usuarios = criar_lista_usuarios()
-    email, senha = usuario_logado #retorna os valores de email e senha como tuplas do usuario
-    for usuario in usuarios:
-        if usuario["email"] == email and usuario["senha"] == senha:
+    for usuario in range (len(usuarios)):
+        usuario_dict = ast.literal_eval(usuarios[usuario])
+        if usuario_dict['email'] == email and usuario_dict['senha'] == senha:
             print("Usuário logado! Bem-vindo!")
+            arquivo = open("filmes_usuarios.txt","w")
             return True
+        
     print("Usuário não encontrado... tente novamente!")
     return False
 
-#-------------------------------------------------------------------------------------------
+#---------filmes----------------------------------------------------------------------------
+
+def catalogo():
+    l = []
+    arquivo = open("filmes.txt","r")
+    arquivo.seek(0)
+    for linha in arquivo.readlines():
+        l.append(linha)
+        print(linha)
+    arquivo.close()
+    return l
+
+def procura_filme(filme,f):
+    if 1 <= filme <= len(f):
+        print(f[filme - 1])
+    else:
+        print("filme invalido!")
+
+#-------codigo principal--------------------------------------------------------------------
 
 inicio = input("digite 1 para cadastro, 2 para login ou enter para sair..: ")
 if inicio == "1" or inicio == "2":
@@ -52,8 +70,16 @@ if inicio == "1" or inicio == "2":
         adicionar_usuario(usuario_criado)
         print("usuario criado com sucesso!")
     while True:
-        usuario_logado = login_usuario()
-        if checagem_login(usuario_logado):
+        if checagem_login():
+            break
+    while True:
+        gerencia = input("digite 1 para pesquisar, 2 para gerenciariamento e 3 para sair..: ")
+        if gerencia == "1":
+            f = catalogo()
+            filme = int(input("digite o numero do filme dentro do nosso catalogo(1 a 30)..: "))
+            procura_filme(filme,f)
+        elif gerencia == "3":
+            print("obrigado por usar nosso programa!")
             break
 else:
     print("obrigado por usar nosso programa!")     
